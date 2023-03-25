0e98f72145a69f3520dd58183d101dadb9dc3111
Matheus_Barreto <matheusbarreto.dev@gmail.com>
2023-03-24 23:44:40 -0300

    arquivo principal
    Arquivo principal com conexão aos arquivos do bootstrap

---
+++ source/index.php
@@ -0,0 +1,66 @@
+<!doctype html>
+<html lang="pt-BR">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Bootstrap demo</title>
+  <!--Bootstrap CSS-->
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="index.php">Faculdade CDL</a>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link" href="?page=novo">Cadastro</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="?page=listar">Lista</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <div class="">
+    <div class="row">
+      <div class="col mt-5">
+        <!--incluindo as paginas secundarias a pagina inicial-->
+        <?php
+        include("config.php");
+        /*--declarando variavel--*/
+        switch (@$_REQUEST["page"])
+          /*--casos de escolha--*/{
+          case "novo":
+            include("cadastro_aluno.php");
+            break;
+          case "listar":
+            include("listar_aluno.php");
+            break;
+        }
+        ?>
+      </div>
+    </div>
+  </div>
+
+
+
+
+
+
+
+
+
+
+  <script src="js/bootstrap.bundle.min.js">
+
+
+  </script>
+</body>
+
+</html>

17eeb20d65f911dafd89210db9b108065c88c523
Matheus_Barreto <matheusbarreto.dev@gmail.com>
2023-03-26 20:43:20 -0300

    novo arquivo de listagem
    select no banco e listando os alunos inscritos

--- source/listar_aluno.php
+++ source/listar_aluno.php
@@ -1,1 +1,43 @@
-<h1>listar_aluno</h1>
+<form class="container">
+<h1>Alunos Cadastrado</h1>
+<?php 
+    /*--passando select e executando--*/
+    $sql = "SELECT * FROM alunos";
+    $res = $conn->query($sql);
+
+    /*--pegando do res a quantidade de linhas--*/
+    $qtd = $res->num_rows;
+
+    if($qtd>0){
+        print "<table class='table table-houver table-striped table-bordered'>";
+        print "<tr>";
+            print "<tr>";
+            print "<th>#</th>";
+            print "<th>NOME</th>";
+            print "<th>E-MAIL</th>";
+            print "<th>CPF</th>";
+            print "<th>Endereço</th>";
+            print "<th>Ações</th>";
+            print "</tr>";
+
+        /*--loop para impressão do select--*/
+        while($row = $res->fetch_object()){
+            print "<tr>";
+            print "<td>".$row->id."</td>";
+            print "<td>".$row->nomeCompleto."</td>";
+            print "<td>".$row->email."</td>";
+            print "<td>".$row->cpf."</td>";
+            print "<td>".$row->endereco."</td>";
+            print "<td>
+                    <button class='btn btn-success'>Editar</button>
+                    <button onclick=\"location.href='editar_aluno.php?id=".$row->id."';\" class='btn btn-danger'>Excluir</button>
+                  </td>";
+            print "</tr>";
+        }
+        print("</table>");
+
+    }else{
+        print "<p class='alert alert-danger'>Aluno não encontrado!</p>";
+    }
+?>
+</form>

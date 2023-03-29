746dfa622fc4f080d8c94eb89ebcf420ccec947c
Matheus_Barreto <matheusbarreto.dev@gmail.com>
2023-03-29 16:38:42 -0300

    Desenvolvimento Editar
    desenvolvimento da ação do botão editar

--- source/salvar_aluno.php
+++ source/salvar_aluno.php
@@ -27,6 +27,27 @@ switch (@$_REQUEST["acao"]){
         break;
     /*--UPDATE--*/
     case "editar":
+        $nome = $_POST["nome"];
+        $email = $_POST["email"];
+        $cpf = $_POST["cpf"];
+        $endereco = $_POST["endereco"];
+
+        $sql = "UPDATE alunos SET 
+                        nomeCompleto = '{$nome}', 
+                        email = '{$email}', 
+                        cpf = '{$cpf}', 
+                        endereco = '{$endereco}'
+                    WHERE 
+                        id =".$_REQUEST["id"];
+        /*--executando a query apartir da variavel declarada no conf.php--*/
+        $res = $conn->query($sql);
+
+        if($res==true){
+            print "<script>alert('Editado com sucesso!');</script>";
+        }
+        else{
+            print "<script>alert('Edição sem sucesso!');</script>";
+        }
         break;
     /*--DELETE--*/
     case "excluir":

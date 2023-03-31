eaa6dd9889104ffd411c6f09c4838e58a46dba65
Matheus de Assis <assis4002@gmail.com>
2023-03-30 23:44:44 -0300

    ajustes

---
+++ source/apresentacao.php
@@ -0,0 +1,11 @@
+<div class="jumbotron bg-white text-center">
+  <h1 class="display-4">Equipe 2: PHP com banco de dados</h1>
+  <p class="lead">Projeto: Protótipo de sistema de cadastro de alunos (cadastro, listagem, edição e deleção).</p>
+  <hr class="my-4 w-50">
+  <p>Membros: Gabriel, Matheus de Assis, Potiguara, Jéssica, Fulano, Beltrano.</p>
+  <div class="mt-5">
+    <p class="lead">Cadastre um usuário ou consulte os existentes:</p>
+    <a class="btn btn-outline-primary btn-lg" href="?page=novo" role="button">Cadastrar</a>
+    <a class="btn btn-outline-success btn-lg" href="?page=listar" role="button">Consultar</a>
+  </div>
+</div>
--- source/listar_aluno.php
+++ source/listar_aluno.php
@@ -1,42 +1,42 @@
-<form class="container">
-<h1>Alunos Cadastrado</h1>
-<?php 
-    /*--passando select e executando--*/
-    $sql = "SELECT * FROM alunos";
-    $res = $conn->query($sql);
+<div class="container">
+    <h1>Alunos Cadastrados</h1>
+    <?php 
+        /*--passando select e executando--*/
+        $sql = "SELECT * FROM alunos";
+        $res = $conn->query($sql);
 
-    /*--pegando do res a quantidade de linhas--*/
-    $qtd = $res->num_rows;
+        /*--pegando do res a quantidade de linhas--*/
+        $qtd = $res->num_rows;
 
-    if($qtd>0){
-        print "<table class='table table-houver table-striped table-bordered'>";
-        print "<tr>";
+        if($qtd>0){
+            print "<table class='table table-houver table-striped table-bordered mt-5'>";
             print "<tr>";
-            print "<th>#</th>";
-            print "<th>NOME</th>";
-            print "<th>E-MAIL</th>";
-            print "<th>CPF</th>";
-            print "<th>Endereço</th>";
-            print "<th>Ações</th>";
-            print "</tr>";
+                print "<tr>";
+                print "<th>#</th>";
+                print "<th>NOME</th>";
+                print "<th>E-MAIL</th>";
+                print "<th>CPF</th>";
+                print "<th>Endereço</th>";
+                print "<th>Ações</th>";
+                print "</tr>";
 
-        /*--loop para impressão do select--*/
-        while($row = $res->fetch_object()){
-            print "<tr>";
-            print "<td>".$row->id."</td>";
-            print "<td>".$row->nomeCompleto."</td>";
-            print "<td>".$row->email."</td>";
-            print "<td>".$row->cpf."</td>";
-            print "<td>".$row->endereco."</td>";
-            print "<td>
-                    <a href='?page=editar&id=$row->id' class='btn btn-success'>Editar</a>
-                    <a href='?page=excluir&id=$row->id' class='btn btn-danger'>Excluir</button>
-                  </td>";
-            print "</tr>";
+            /*--loop para impressão do select--*/
+            while($row = $res->fetch_object()){
+                print "<tr>";
+                print "<td>".$row->id."</td>";
+                print "<td>".$row->nomeCompleto."</td>";
+                print "<td>".$row->email."</td>";
+                print "<td>".$row->cpf."</td>";
+                print "<td>".$row->endereco."</td>";
+                print "<td>
+                        <a href='?page=editar&id=$row->id' class='btn btn-success'>Editar</a>
+                        <a href='?page=excluir&id=$row->id' class='btn btn-danger'>Excluir</button>
+                    </td>";
+                print "</tr>";
+            }
+            print("</table>");
+        }else{
+            print "<p class='alert alert-danger'>Aluno não encontrado!</p>";
         }
-        print("</table>");
-    }else{
-        print "<p class='alert alert-danger'>Aluno não encontrado!</p>";
-    }
-?>
-</form>
+    ?>
+</div>

e8b8166b271ccf2af495ca9f35e57c93d666d190
Matheus_Barreto <matheusbarreto.dev@gmail.com>
2023-06-25 21:18:49 -0300

    Update listar_aluno.php

--- source/listar_aluno.php
+++ source/listar_aluno.php
@@ -11,32 +11,32 @@
         if($qtd>0){
             print "<a class='btn btn-outline-primary btn mt-4' href='?page=novo'>Cadastrar</a>";
             print "<table class='table table-houver table-striped table-bordered mt-3'>";
-            print "<tr>";
                 print "<tr>";
-                print "<th>#</th>";
-                print "<th>NOME</th>";
-                print "<th>E-MAIL</th>";
-                print "<th>CPF</th>";
-                print "<th>Endereço</th>";
-                print "<th>Ações</th>";
-                print "</tr>";
+                    print "<tr>";
+                    print "<th>#</th>";
+                    print "<th>NOME</th>";
+                    print "<th>E-MAIL</th>";
+                    print "<th>CPF</th>";
+                    print "<th>Endereço</th>";
+                    print "<th>Ações</th>";
+                    print "</tr>";
 
-            $index = 0;
-            
-            /*--loop para impressão do select--*/
-            while($row = $res->fetch_object()){
-                print "<tr>";
-                print "<td>".++$index."</td>";
-                print "<td>".$row->nomeCompleto."</td>";
-                print "<td>".$row->email."</td>";
-                print "<td>".$row->cpf."</td>";
-                print "<td>".$row->endereco."</td>";
-                print "<td>
-                        <a href='?page=editar&id=$row->id' class='btn btn-success'>Editar</a>
-                        <a href='?page=excluir&id=$row->id' class='btn btn-danger'>Excluir</button>
-                    </td>";
-                print "</tr>";
-            }
+                $index = 0;
+                
+                /*--loop para impressão do select--*/
+                while($row = $res->fetch_object()){
+                    print "<tr>";
+                    print "<td>".++$index."</td>";
+                    print "<td>".$row->nomeCompleto."</td>";
+                    print "<td>".$row->email."</td>";
+                    print "<td>".$row->cpf."</td>";
+                    print "<td>".$row->endereco."</td>";
+                    print "<td>
+                            <a href='?page=editar&id=$row->id' class='btn btn-success'>Editar</a>
+                            <a href='?page=excluir&id=$row->id' class='btn btn-danger'>Excluir</button>
+                        </td>";
+                    print "</tr>";
+                }
             print("</table>");
         }else{
             print "<p class='alert alert-danger mt-5'>Nenhum aluno foi encontrado!</p>";
